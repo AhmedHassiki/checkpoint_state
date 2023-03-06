bdbd4d5387dc52be1716331caa36fa31885a7585
Ahmed Hassiki <hassiki.services@gmail.com>
2023-03-06 14:22:48 +0100

    checkpoint_state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  state = {
+    fullName: "Ahmed Hassiki",
+    bio: "lorem ipsum tararara",
+    imgSrc : "http://www.gomycode.com/images.png",
+    profession : "Entrepreneur",
+    isShow:true,
+    timeElapsed : 0
+  };
+  
+
+  componentDidMount() {
+    this.startTime = Date.now();
+    this.interval = setInterval(() => {
+      this.setState({ timeElapsed: Math.floor((Date.now() - this.startTime) / 1000) });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+
+  toggleShow = () => {
+    this.setState({ isShow: !this.state.isShow })
+  };
+  render() {
+    return (
+      <>
+      {this.state.isShow ? <h2>{this.state.fullName}</h2> : null}
+      <button className='thebutton' onClick={this.toggleShow}>Toggle</button>
+      <p>Time elapsed since component mounted: {this.state.timeElapsed} seconds</p>
+
+      </>
+
   );
+}
 }
 
 export default App;
